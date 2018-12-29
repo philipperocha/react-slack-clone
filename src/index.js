3bfe1b12c8ff6c0ea47d18c992ba3aeb4d3083b5
Philippe <philippe2008@hotmail.com>
2018-12-29 16:48:00 -0300

    Update Register Screen.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-
 import registerServiceWorker from './registerServiceWorker';
+
+import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -21,5 +21,5 @@ const Root = () => {
 	)
 }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root/>, document.getElementById('root'));
 registerServiceWorker();
