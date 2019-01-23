15df8d8e04cd8cfaa4dc592e58a5c19aaf367b7a
Philippe <philippe2008@hotmail.com>
2019-01-23 09:43:38 -0300

    Put CurrentChannel on GlobalState.

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { setCurrentChannel } from '../../actions';
 import firebase from '../../firebase';
 
 class Channels extends Component {
@@ -63,11 +65,15 @@ class Channels extends Component {
 		this.setState({ [event.target.name]: event.target.value });
 	};
 
+	changeChannel = channel => {
+		this.props.setCurrentChannel(channel);
+	}
+
 	displayChannels = channels => (
 		channels.length > 0 && channels.map(channel => (
 			<Menu.Item
 				key={channel.id}
-				onClick={() => console.log(channel)}
+				onClick={() => this.changeChannel(channel)}
 				name={channel.name}
 				style={{ opacity: 0.7 }}
 			>
@@ -137,4 +143,7 @@ class Channels extends Component {
 	}
 }
 
-export default Channels;
+export default connect(
+	null, 
+	{ setCurrentChannel }
+)(Channels);
