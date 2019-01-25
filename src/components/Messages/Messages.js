9da52d733c0194780cc3a6848938bbdf7acd66b8
Philippe <philippe2008@hotmail.com>
2019-01-25 00:18:31 -0300

    Display Messages. Add Message Component.

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,23 +4,59 @@ import firebase from '../../firebase';
 
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
+import Message from './Message';
 
 class Messages extends Component {
 	state = {
+		messages: [],
+		messagesLoading: true,
 		messagesRef: firebase.database().ref('messages'),
 		channel: this.props.currentChannel,
 		user: this.props.currentUser
-	}
+	};
+
+	componentDidMount() {
+		const { channel, user } = this.state;
+
+		if (channel && user) {
+			this.addListeners(channel.id);
+		}
+	};
+
+	addListeners = channelId => {
+		this.addMessageListener(channelId);
+	};
+
+	addMessageListener = channelId => {
+		const loadedMessages = [];
+		this.state.messagesRef.child(channelId).on('child_added', snap => {
+			loadedMessages.push(snap.val());
+			this.setState({
+				messages: loadedMessages,
+				messagesLoading: false
+			});
+		});
+	};
+
+	displayMessages = messages => (
+		messages.length > 0 && messages.map(message => (
+			<Message
+				key={message.timestamp}
+				message={message}
+				user={this.state.user}
+			/>
+		))
+	);
 
 	render() {
-		const { messagesRef, channel, user } = this.state;
+		const { messagesRef, messages, channel, user } = this.state;
 
 		return (
 			<React.Fragment>
 				<MessagesHeader />
 				<Segment>
 					<Comment.Group className='messages'>
-
+						{this.displayMessages(messages)}
 					</Comment.Group>
 				</Segment>
 
@@ -31,7 +67,7 @@ class Messages extends Component {
 				/>
 			</React.Fragment>
 		)
-	}
+	};
 }
 
 export default Messages;
